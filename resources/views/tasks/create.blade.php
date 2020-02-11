6c67f8850bef3077f25a7fc4d5c970eedfe79a36
KIMINJOONG <ijboym216@gmail.com>
2020-02-11 09:09:58 +0900

    make to create tasks form

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,4 +4,13 @@
 
 @section('content')
     <h1 class="font-bold text-3xl">Create Task</h1>
+    <form action="/tasks" method="POST">
+        <label class="block" for="title">Title</label>
+        <input class="border border-gray-800" type="text" name="title" id="title" />
+
+        <label class="block" for="body">Body</label>
+        <textarea class="border border-gray-800" name="body" id="body" cols="30" rows="10"></textarea>
+
+        <button class="bg-blue-600">Submit</button>
+    </form>
 @endsection
