d7068aff12c978f79d1d0575caaab3622cf92a90
KIMINJOONG <ijboym216@gmail.com>
2020-02-18 09:03:42 +0900

    add header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}" />
 </head>
 <body>
     <div id="app">
